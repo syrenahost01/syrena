1f629e27b5523622891c0f64d002d15ad01a23a9
syrenahost01 <46450478+syrenahost01@users.noreply.github.com>
2019-01-10 13:21:39 +0100

    Add files via upload

---
+++ syrena-tablette/syrena-sna-tablette-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source .env
+
+sudo docker import ../docker-build/grafana/grafana-${GRAFANA_VERSION}-plugins.tar
+
+sudo docker-compose up --no-start
+
+sudo docker container start ${CONTAINER_NAMESPACE}-kafka-zookeeper
+
+sudo docker container start ${CONTAINER_NAMESPACE}-kafka-broker
+
+while [ "`sudo docker inspect -f {{.State.Running}} ${CONTAINER_NAMESPACE}-kafka-zookeeper`" != "true" ]; do sleep 15; done
+while [ "`sudo docker inspect -f {{.State.Running}} ${CONTAINER_NAMESPACE}-kafka-broker`" != "true" ]; do sleep 15; done
+sleep 15
+sudo docker exec -it ${CONTAINER_NAMESPACE}-kafka-zookeeper kafka-topics --create --topic ${LOCAL_TOPIC} --partitions 1 --replication-factor 1 --if-not-exists --zookeeper ${CONTAINER_NAMESPACE}-kafka-zookeeper:2181
+sudo docker exec -it ${CONTAINER_NAMESPACE}-kafka-zookeeper kafka-topics --create --topic ${REMOTE_TOPIC} --partitions 1 --replication-factor 1 --if-not-exists --zookeeper ${CONTAINER_NAMESPACE}-kafka-zookeeper:2181
+
+sudo docker container start ${CONTAINER_NAMESPACE}-influxdb
+while [ "`sudo docker inspect -f {{.State.Running}} ${CONTAINER_NAMESPACE}-influxdb`" != "true" ]; do sleep 15; done
+sleep 15
+sudo docker exec -it ${CONTAINER_NAMESPACE}-influxdb influx -execute 'create DATABASE syrena' -precision rfc3339
+
+sudo docker container start ${CONTAINER_NAMESPACE}-telegraf
+
+sudo docker container start ${CONTAINER_NAMESPACE}-grafana
