62c23b4668261c3d3f75a4f01d85b347c1a11d39
syrenahost01 <46450478+syrenahost01@users.noreply.github.com>
2019-01-10 14:09:25 +0100

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -13,4 +13,9 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 sudo docker --version
+sudo docker-compose --version
