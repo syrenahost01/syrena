9c6dbf656f7442da13062e4546e2fdbe0a8acda2
syrenahost01 <46450478+syrenahost01@users.noreply.github.com>
2019-01-19 08:55:05 +0100

    Update syrena-install.sh

--- syrena-central/syrena-install.sh
+++ syrena-central/syrena-install.sh
@@ -13,7 +13,7 @@ sudo docker container start ${CONTAINER_NAMESPACE}-kafka-broker
 while [ "`sudo docker inspect -f {{.State.Running}} ${CONTAINER_NAMESPACE}-kafka-zookeeper`" != "true" ]; do sleep 15; done
 while [ "`sudo docker inspect -f {{.State.Running}} ${CONTAINER_NAMESPACE}-kafka-broker`" != "true" ]; do sleep 15; done
 sleep 15
-sudo docker exec -it ${CONTAINER_NAMESPACE}-kafka-zookeeper kafka-topics --create --topic ${LOCAL_TOPIC} --partitions 1 --replication-factor 1 --config cleanup.policy=${KAFAKA_CLEANUP_POLICY} --config compression.type=${KAFKA_COMPRESSION_TYPE} --retention.ms=${KAFKA_RETENTION_MS} --if-not-exists --zookeeper ${CONTAINER_NAMESPACE}-kafka-zookeeper:2181
+sudo docker exec -it ${CONTAINER_NAMESPACE}-kafka-zookeeper kafka-topics --create --topic ${LOCAL_TOPIC} --partitions 1 --replication-factor 1 --config cleanup.policy=${KAFAKA_CLEANUP_POLICY} --config compression.type=${KAFKA_COMPRESSION_TYPE} --config retention.ms=${KAFKA_RETENTION_MS} --if-not-exists --zookeeper ${CONTAINER_NAMESPACE}-kafka-zookeeper:2181
 
 sudo docker container start ${CONTAINER_NAMESPACE}-influxdb
 while [ "`sudo docker inspect -f {{.State.Running}} ${CONTAINER_NAMESPACE}-influxdb`" != "true" ]; do sleep 15; done
